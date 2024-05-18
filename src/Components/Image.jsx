1110d7a2c1600309eadb6ab845d8894efddc691e
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2024-05-18 10:23:02 +0530

    changed styling for generate page

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -42,21 +42,39 @@ function FirstImage() {
     }
   };
 
+  const buttonStyle = {
+    border: "1px solid #ccc",
+    padding: "12px 24px",
+    borderRadius: "4px",
+    cursor: "pointer",
+    color: "white",
+    fontSize: "16px",
+    backgroundColor: "darkblue",
+    marginTop: "20px",
+  };
+
   return (
     <div className="App">
-      {/* <div className="box"> */}
       <div className="main">
+        {!image && (
           <form onSubmit={handleSubmit}>
-            <button type="submit">Generate</button>
+            <button type="submit" style={buttonStyle}>Generate</button>
           </form>
+        )}
         {image !== null && (
-          <img src={image} height="370" />
+          <img src={image} height="396" />
         )}
       </div>
       <div className="box">
-        <input type="file" onChange={handlePersonChange} />
+        <label className="fileInputLabel" style={buttonStyle}>
+          Choose Image
+          <input type="file" onChange={handlePersonChange} style={{ display: "none" }} />
+        </label>
         <img alt="" src={person !== null ? URL.createObjectURL(person) : undefined} width="200" height="200" />
-        <input type="file" onChange={handleClothChange} />
+        <label className="fileInputLabel" style={buttonStyle}>
+          Choose Clothing
+          <input type="file" onChange={handleClothChange} style={{ display: "none" }} />
+        </label>
         <img alt="" src={cloth !== null ? URL.createObjectURL(cloth) : undefined} width="200" height="200" />
       </div>
     </div>
